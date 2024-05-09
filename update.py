eb4228bf8744d77a8dd573ecb694c1defcc7bc54
Marcos Vinicius <devmarcoscoelho@gmail.com>
2024-05-09 18:35:23 -0300

    bug fix

--- update.py
+++ update.py
@@ -5,51 +5,81 @@ import subprocess
 import zipfile
 import os
 import sys
+from pathlib import Path 
 
-def atualizar_programa():
+
+#informação da versão
+response = requests.get('https://api.github.com/repos/SethiCoel/atualizacao-bot-wpp/releases/latest')
+latest_release = json.loads(response.text)
+ultima_versao = latest_release['tag_name']
+
+nome_do_arquivo = latest_release['assets'][0]['name']
+download_url = latest_release['assets'][0]['browser_download_url']
+
+
+def versao():
+
+    with open('versao.txt', 'r', encoding='utf-8') as arquivo:
+        texto = arquivo.read()
+        
+    return texto
+
+def download_file():
     try:
-        response = requests.get('https://api.github.com/repos/SethiCoel/atualizacao-bot-wpp/releases/latest')
-        latest_release = json.loads(response.text)
-        ultima_versao = latest_release['tag_name']
 
+        if ultima_versao != versao():
 
-        nome_do_arquivo = latest_release['assets'][0]['name']
-        download_url = latest_release['assets'][0]['browser_download_url']
-        download_file(download_url, nome_do_arquivo)
+            print('Baixando nova versão...')
+            sleep(1)
+
+            print('Atualizando programa...')
+
+            response = requests.get(download_url)
+            with open(nome_do_arquivo, 'wb') as f:
+                f.write(response.content)
+
+            print('Atualização Finalizada!')
+            
+    except Exception as error:
+        print(error)
+        input()
+    
+
+def extrair_arquivo():
+    try:
+        diretorio_atual = os.getcwd()
+        diretorio_anterior = os.path.dirname(diretorio_atual)
+
+        arquivo = Path(nome_do_arquivo)
+
+        if arquivo.exists():
+
+            with zipfile.ZipFile(nome_do_arquivo, 'r') as zip_ref:
+                zip_ref.extractall(diretorio_atual)
+            
+            caminho_arquivo = f'{diretorio_atual}/{nome_do_arquivo}'
+            
+
+            sleep(2)
+            os.remove(caminho_arquivo)
 
     except Exception as error:
         print(error)
+        input()
 
-def download_file(url, nome):
-    print('Baixando nova versão...')
-    sleep(1)
-
-    print('Atualizando programa...')
-
-    diretorio_atual = os.getcwd()
-    response = requests.get(url)
-    with open(nome, 'wb') as f:
-        f.write(response.content)
-
-    print('Atualização Finalizada!')
-
-    extrair_arquivo(nome, diretorio_atual)
-
-def extrair_arquivo(arquivo_zip, extrair_para):
-    with zipfile.ZipFile(arquivo_zip, 'r') as zip_ref:
-        zip_ref.extractall(extrair_para)
-    
-    sleep(1)
-    os.remove(arquivo_zip)
 
 def abrir_programa():
     sleep(1)
+
     caminho_executavel = f'Mensagem.Automatica.exe'
+
     command = f'start {caminho_executavel}'
     subprocess.Popen(command, shell=True)
     sys.exit()
 
+
 if __name__ == '__main__':
-    atualizar_programa()
-    abrir_programa()
+    download_file()
+    extrair_arquivo()
+    # abrir_programa()
 
